59c9bd9a2e42a13b72e1a5653e623438a5918c9b
rall138 <ricardo.lomez@gmail.com>
2022-11-18 14:46:23 -0300

    Subiendo todo

---
+++ controller/carritoController.js
@@ -0,0 +1,71 @@
+const express = require('express')
+const PersistenciaArchivos = require('../persistence/persistenciaArchivos')
+
+class carritoController{
+
+    constructor(){
+
+        this.carritoRouter = express.Router()
+        this.persistencia = new PersistenciaArchivos('carritos.txt')
+
+        this.carritoRouter.get('/carrito', (req, res) =>{
+            this.persistencia.obtenerTodos()
+            .then((result) => res.send(result))
+            .catch(error => res.json(error))
+        })
+
+        this.carritoRouter.get('/carrito/:id', (req, res) =>{
+            this.persistencia.obtengoItem(req.params.id)
+            .then((result) => res.json(result.productos))
+            .catch(error => res.json(error))
+        })
+
+        this.carritoRouter.post('/carrito', (req, res) =>{
+            const carrito = req.body
+            carrito.timestamp = Date.now()
+            this.persistencia.insertarItem(carrito)
+            .then((result) => res.json(result))
+            .catch(error => res.json(error))
+        })
+
+        // Agregar productos al carro
+        this.carritoRouter.post('/carrito/:id/productos', (req, res) =>{
+            const producto = req.body
+            this.persistencia.obtengoItem(req.params.id)
+            .then(carrito => {
+                carrito.productos.push(producto)
+                return this.persistencia.modificarItem(carrito.id, carrito)
+            })
+            .then(status => res.json(status))
+        })
+
+        this.carritoRouter.put('/carrito/:id', (req, res) =>{
+            this.persistencia.modificarItem(req.params.id, req.body)
+            .then((result) => res.json(result))
+            .catch(error => res.json(error))
+        })
+
+        this.carritoRouter.delete('/carrito/:id', (req, res) =>{
+            this.persistencia.eliminarItem(req.params.id)
+            .then((result) => res.json(result))
+            .catch(error => res.json(error))
+        })
+
+        this.carritoRouter.delete('/carrito/:id/productos/:id_prod', (req, res) =>{
+            this.persistencia.obtengoItem(req.params.id)
+            .then(carrito => {
+                carrito.productos = carrito.productos.filter(prd => parseInt(prd.id) !== parseInt(req.params.id_prod))
+                return this.persistencia.modificarItem(carrito.id, carrito)
+            })
+            .then(status => res.json(status))
+        })
+
+    }
+
+    getRouter(){
+        return this.carritoRouter
+    }
+
+}
+
+module.exports = carritoController
---
+++ controller/productosController.js
@@ -0,0 +1,49 @@
+const express = require('express')
+const PersistenciaArchivos = require('../persistence/persistenciaArchivos')
+
+class productosController{
+
+    constructor(){
+
+        this.productosRouter = express.Router()
+        this.persistencia = new PersistenciaArchivos('productos.txt')
+
+        this.productosRouter.get('/productos', (req, res) =>{
+            this.persistencia.obtenerTodos()
+            .then((result) => res.send(result))
+            .catch(error => res.json(error))
+        })
+
+        this.productosRouter.get('/productos/:id', (req, res) =>{
+            this.persistencia.obtengoItem(req.params.id)
+            .then((result) => res.json(result))
+            .catch(error => res.json(error))
+        })
+
+        this.productosRouter.post('/productos', (req, res) =>{
+            this.persistencia.insertarItem(req.body)
+            .then((result) => res.json(result))
+            .catch(error => res.json(error))
+        })
+
+        this.productosRouter.put('/productos/:id', (req, res) =>{
+            this.persistencia.modificarItem(req.params.id, req.body)
+            .then((result) => res.json(result))
+            .catch(error => res.json(error))
+        })
+
+        this.productosRouter.delete('/productos/:id', (req, res) =>{
+            this.persistencia.eliminarItem(req.params.id)
+            .then((result) => res.json(result))
+            .catch(error => res.json(error))
+        })
+
+    }
+
+    getRouter(){
+        return this.productosRouter
+    }
+
+}
+
+module.exports = productosController
